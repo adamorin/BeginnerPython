4c66e4fe83f64ce9b3d3b8fb1e9d307db41ec979
radioman1981 <radioman1981@applebit.com>
2019-02-25 23:23:44 -0800

    Numpy

--- scratch.py
+++ scratch.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import numpy
+import numpy as np
 from scipy import stats
 import smirnov_grubbs as grubbs
-
+'''
 reads = [[1,2,3],[1.1, 2.1, 3.1],[7,8,9]]
 reads1 = [1,1,100]
 ids = [11, 12, 1300]
@@ -15,3 +15,10 @@ z1 = stats.zscore([[1,1,1,1],[1.1,1.1,1.1,1.1],[1,1,1,1],[1.3,1.3,1.3,1.13],[10,
 data = pd.Series([1, 8, 9, 10, 9])
 newdata = grubbs.test(data, alpha=0.05)
 outlier = grubbs.min_test_indices([240,240.1, 242, 230, 244], alpha=0.05)
+
+
+'''
+A = [[1,2,3],[4,5,6],[7,8,9]]
+b = np.array(A)
+for col in b.T:
+    print(col)
